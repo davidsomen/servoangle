47d15512947e30b838a1ccd12882b0417b481074
David Somen <davidsomen@me.com>
2013-04-06 16:22:28 +0000

    Create servoanglelib.h

---
+++ servoanglelib.h
@@ -0,0 +1,10 @@
+#ifndef SERVOANGLELIB_H
+#define SERVOANGLELIB_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+extern int servoAngle(char *servo, char *angle);
+extern int servoOff(char *servo);
+
+#endif
